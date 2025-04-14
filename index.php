53755c33c8815a8ef792c287d89518d1af62952f
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-04-14 16:51:04 -0300

    Modified background

--- index.php
+++ index.php
@@ -4,10 +4,31 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Book Wise</title>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-  <h1>
-    
-  </h1>
+
+<!-- Body of Page -->
+<body class="bg-stone-950 text-stone-400">
+  <header class="bg-stone-800">
+    <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+
+      <div> Book Wise </div>
+
+      <ul class="flex">
+        <li><a href="">Explore</a></li>
+        <li><a href="/my_books">My Books</a></li>
+      </ul>
+
+      <ul>
+        <li><a href="/login"></a>Login</li>
+      </ul>
+
+    </nav>
+  </header>
+
+  <main>
+    Book List
+  </main>
+
 </body>
 </html>
