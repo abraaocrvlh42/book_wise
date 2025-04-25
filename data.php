9bf6dbe292eb6686e8ef26741e277f250e125232
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-04-24 21:47:10 -0300

    Modified

---
+++ data.php
@@ -0,0 +1,21 @@
+<?php
+  $books = [
+    ['id' => 1, 'title' => 'The Lord of the Rings', 'author' => 'J.R.R Tolkien', 'description' => 'Set in Middle-earth, the story began as a sequel to book The Hobbit but eventually developed into a much larger work.'],
+
+    ['id' => 2, 'title' => 'Neuromancer', 'author' => 'William Gibson', 'description' => 'Case, a computer hacker enlisted into a crew by a powerful artificial intelligence and a traumatised former soldier to complete a high-stakes heist.'],
+
+    ['id' => 3, 'title' => "The Hitchhiker's Guide to the Galaxy", 'author' => 'Douglas Adams', 'description' => "The Hitchhiker's Guide to the Galaxy is a sci-fi comedy by Douglas Adams that follows Arthur Dent's bizarre adventures through space after Earth is destroyed."],
+    
+    ['id' => 4, 'title' => 'The Short Legs of Lies', 'author' => 'Moacyr Scliar', 'description' => "The human imagination can be used to create new and fascinating ways of feeling and acting, as well as to invent lies, from the most innocent to the most dangerous slander."],
+
+    ['id' => 5, 'title' => 'The Bookseller of Kabul', 'author' => 'Åsne Seierstad', 'description' => 'An intimate portrait of a man, his principles and his family, two wives, five children and many relatives sharing a small four-room apartment.'],
+
+    ['id' => 6, 'title' => 'Les Misérables', 'author' => 'Victor Hugo', 'description' => 'Les Misérables is a French epic historical novel by Victor Hugo, first published on 31 March 1862, that is considered one of the greatest novels of the 19th century.'],
+
+    ['id' => 7, 'title' => 'Dom Casmurro', 'author' => 'Joaquim Maria Machado de Assis', 'description' => 'Dom Casmurro is an 1899 novel written by Brazilian author Joaquim Maria Machado de Assis.'],
+
+    ['id' => 8, 'title' => 'Lolita', 'author' => 'Vladimir Nabokov', 'description' => 'The protagonist is a French literature professor who moves to New England and writes under the pseudonym Humbert Humbert.'],
+    
+    ['id' => 9, 'title' => 'The Little Prince', 'author' => 'Antoine de Saint-Exupéry', 'description' => "It's a poetic and philosophical story that tells of the encounter between a pilot lost in the desert and a young prince from a small asteroid."]
+  ];
+?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  require 'data.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
