3e97f336aa97fcb3a1b4848167ed94f8318a90bb
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-04-30 14:43:43 -0300

    Books created

--- books.php
+++ books.php
@@ -39,21 +39,28 @@
   </header>
 
   <main class="mx-auto max-w-screen-lg space-y-6">
+    
     ID do Livro é: <?= $books['title'] ?>
+    
     <div class=" p-2 rounded border-stone-800 border-2 bg-stone-900">
       <div class="flex">
-        <div class="w-1/3">Images</div>
+        
+        <div class="w-1/3">
+          Images
+        </div>
         
         <div class='space-y-1'>
           <a href="/books.php?id=<?=$book['id']?>" class="font-semibold hover-underline"><?= $book['title']?></a>
-          <div class="text-xs italic"><?= $book['author'] ?></div>
+          <div class="text-xs italic"><?=$book['author'] ?></div>
           <div class="text-xs italic">3 (Reviews)</div>
         </div>
       </div>
+      
       <div class="text-sm mt-2">
         <?=$book['description']?>
       </div>
     </div>
+  
   </main>
 
 </body>
